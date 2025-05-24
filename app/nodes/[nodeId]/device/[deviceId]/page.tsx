4b6eb1ffa970b101d27aaeab67afb06f228e2104
ITea0fc <ilyatarasov@bk.ru>
2025-05-25 01:37:49 +0700

    add static params

--- app/nodes/[nodeId]/device/[deviceId]/page.tsx
+++ app/nodes/[nodeId]/device/[deviceId]/page.tsx
@@ -2,3 +2,13 @@
 import { DevicePage } from 'pages/DevicePage'
 
 export default DevicePage
+
+// eslint-disable-next-line
+export async function generateStaticParams() {
+  const posts = await fetch('https://.../posts').then(res => res.json())
+
+  // eslint-disable-next-line
+  return posts.map((post: any) => ({
+    slug: post.slug
+  }))
+}
--- app/nodes/[nodeId]/page.tsx
+++ app/nodes/[nodeId]/page.tsx
@@ -3,3 +3,13 @@
 import { MyDevices } from 'widgets/MyDevices'
 
 export default MyDevices
+
+// eslint-disable-next-line
+export async function generateStaticParams() {
+  const posts = await fetch('https://.../posts').then(res => res.json())
+
+  // eslint-disable-next-line
+  return posts.map((post: any) => ({
+    slug: post.slug
+  }))
+}
