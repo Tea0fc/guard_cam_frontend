afb11511a24482bc95367544fec3a7529e42c8cd
ITea0fc <ilyatarasov@bk.ru>
2025-05-25 00:41:02 +0700

    Add redirect for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+import { redirect } from 'next/navigation'
 import type { FC } from 'react'
-import { memo } from 'react'
+import { memo, useEffect } from 'react'
+
+import { ROUTES } from 'shared/model/const'
 
 const Main: FC = memo(() => {
+  useEffect(() => {
+    redirect(ROUTES.LOGIN)
+  }, [])
+
   return <div>Hello world</div>
 })
 
