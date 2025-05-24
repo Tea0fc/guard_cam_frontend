16333ac0bacc47feff44c3e42db035b193e29ddc
ITea0fc <ilyatarasov@bk.ru>
2025-05-25 01:42:44 +0700

    Fix static functions

--- app/nodes/[nodeId]/device/[deviceId]/page.tsx
+++ app/nodes/[nodeId]/device/[deviceId]/page.tsx
@@ -1,14 +1,9 @@
 'use client'
 import { DevicePage } from 'pages/DevicePage'
 
-export default DevicePage
-
 // eslint-disable-next-line
-export async function generateStaticParams() {
-  const devices = await fetch('https://.../device').then(res => res.json())
-
-  // eslint-disable-next-line
-  return devices.map((device: any) => ({
-    slug: device.slug
-  }))
+export function generateStaticParams() {
+  return [{ slug: [''] }]
 }
+
+export default DevicePage
--- app/nodes/[nodeId]/page.tsx
+++ app/nodes/[nodeId]/page.tsx
@@ -2,14 +2,9 @@
 
 import { MyDevices } from 'widgets/MyDevices'
 
-export default MyDevices
-
 // eslint-disable-next-line
-export async function generateStaticParams() {
-  const nodes = await fetch('https://.../nodes').then(res => res.json())
-
-  // eslint-disable-next-line
-  return nodes.map((node: any) => ({
-    slug: node.slug
-  }))
+export function generateStaticParams() {
+  return [{ slug: [''] }]
 }
+
+export default MyDevices
